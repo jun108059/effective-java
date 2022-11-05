09bce118c95518b16c890f09f81b3c5aa17686c9
YoungJun Park <youngjun108059@gmail.com>
2022-11-05 23:54:37 +0900

    🔥 WIP item41 정리

---
+++ src/main/java/study/yjpark/chapter06/item41/App.java
@@ -0,0 +1,8 @@
+package study.yjpark.chapter06.item41;
+
+public class App {
+    public static void main(String[] args) {
+        Item41 item41 = new Item41();
+        
+    }
+}
---
+++ src/main/java/study/yjpark/chapter06/item41/Item41.java
@@ -0,0 +1,6 @@
+package study.yjpark.chapter06.item41;
+
+import java.io.Serializable;
+
+public class Item41 implements Serializable {
+}
