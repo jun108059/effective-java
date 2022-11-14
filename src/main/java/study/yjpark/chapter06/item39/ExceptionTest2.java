6eb480f57083e2b72a5b87b06ec8fd42ccfceb8f
YoungJun Park <youngjun108059@gmail.com>
2022-11-15 00:02:06 +0900

    ♻️ 에러 수정

--- src/main/java/study/yjpark/chapter06/item39/ExceptionTest2.java
+++ src/main/java/study/yjpark/chapter06/item39/ExceptionTest2.java
@@ -10,5 +10,5 @@ import java.lang.annotation.*;
 @Repeatable(ExceptionTest2Container.class)
 public @interface ExceptionTest2 {
     // 명시한 예외를 던져야만 성공하는 테스트 메서드용 애너테이션
-    Class<? extends Throwable>[] value();
+    Class<? extends Throwable> value();
 }
--- src/main/java/study/yjpark/chapter06/item39/RunTests.java
+++ src/main/java/study/yjpark/chapter06/item39/RunTests.java
@@ -60,4 +60,35 @@ public class RunTests {
         System.out.printf("성공: %d, 실패: %d%n", passed, tests - passed);
     }
 
+    // [3]
+    public static void test3(String[] args) throws Exception {
+        int tests = 0;
+        int passed = 0;
+        Class<?> testClass = Class.forName(args[0]);
+        for (Method m : testClass.getDeclaredMethods()) {
+            if (m.isAnnotationPresent(ExceptionTest2.class) || // @ExceptionTest2 or @ExceptionTest2Container
+                    m.isAnnotationPresent(ExceptionTest2Container.class)) {
+                tests++;
+                try {
+                    m.invoke(null);
+                    System.out.printf("테스트 %s 실패: 예외를 던지지 않음%n", m);
+                } catch (Throwable wrappedExc) {
+                    Throwable exc = wrappedExc.getCause();
+                    int oldPassed = passed;
+                    ExceptionTest2[] excTests = m.getAnnotationsByType(ExceptionTest2.class);
+                    for (ExceptionTest2 excTest : excTests) {
+                        if (excTest.value().isInstance(exc)) {
+                            passed++;
+                            break;
+                        }
+                    }
+                    if (passed == oldPassed) {
+                        System.out.printf("테스트 %s 실패: %s %n", m, exc);
+                    }
+                }
+            }
+        }
+
+        System.out.printf("성공: %d, 실패: %d%n", passed, tests - passed);
+    }
 }
